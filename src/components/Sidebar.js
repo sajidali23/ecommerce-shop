77b40c1430d9cc4f347872c687a88ab88a4bf28d
SAJID ALI <135090212+sajidali23@users.noreply.github.com>
2023-12-23 04:25:20 +0000

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -32,7 +32,7 @@ const Sidebar = () => {
           return <CartItem item={item} key={item.id} />;
         })}
       </div>
-      <div className="flex flex-col gap-y-3 py-4 mt-4">
+      <div className="flex flex-col gap-y-3 py-4">
         <div className="flex w-full justify-between items-center">
           <div className="uppercase font-semibold">
             <span className="mr-2">Total:</span>$ {parseFloat(total).toFixed(2)}
